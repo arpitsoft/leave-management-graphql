513cb440fe1cf7344a87482b7dec256b7b341d69
arpitsoft <arpit.s@neosoftmail.com>
2025-11-24 15:49:14 +0530

    Init

---
+++ server.js
@@ -0,0 +1,19 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+    type Query {
+        greeting:String
+}
+`;
+
+const resolvers = {
+    Query: {
+        greeting: () => 'Hello world!'
+    }
+}
+
+const server = new ApolloServer({typeDefs,resolvers});
+const { url } = await startStandaloneServer(server,{listen:{port:9000}});
+
+console.log(`this server is runing on this port ${url}`);
